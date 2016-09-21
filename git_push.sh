2a854ad8ac8e90562fd1100a5abd033282c1b108
Ben Le Cam <blecam@four51.com>
2016-09-21 10:24:49 -0500

    Fixed README typo

--- git_push.sh
+++ git_push.sh
@@ -33,7 +33,7 @@ if [ "$git_repo_id" = "" ]; then
 fi
 
 if [ "$release_note" = "" ]; then
-    release_note="Bumped gem version to 0.0.3"
+    release_note="Minor update"
     echo "[INFO] No command line input provided. Set \$release_note to $release_note"
 fi
 
--- lib/order_cloud/version.rb
+++ lib/order_cloud/version.rb
@@ -22,5 +22,5 @@ limitations under the License.
 =end
 
 module OrderCloud
-  VERSION = "0.0.3"
+  VERSION = "0.0.2"
 end
